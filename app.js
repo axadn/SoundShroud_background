10f90a4f797c1eb156638048105167dbc2487a72
Adan Marrufo <adanmarrufo@gmail.com>
2018-04-23 17:43:22 -0700

    get preview creator class to output correct results

--- app.js
+++ app.js
@@ -55,9 +55,8 @@ class PreviewGenerator extends stream.Transform{
         this._numChannels = options.numChannels;
         this._numBytes = options.numBytes;
         this._bytesPerSample = options.bytesPerSample;
-        this._resolution = 64;
+        this._resolution = 200;
         this._windowResolution = 8;
-        this._samplesPerWindow = this._numBytes / this._resolution / this._windowResolution;
 
         this._windows = Array(this._windowResolution);
         this.results = Array(this._resolution);
@@ -65,7 +64,9 @@ class PreviewGenerator extends stream.Transform{
         this._windowIdx = 0;
         this._byteNumber = 0;
         this._sample = 0;
+        this._sampleNumber = 0;
         this._windowMax = 0;
+        this._resultIdx = 0;
 
         this._headerSection = 0;
         this._headerOffset = 0;
@@ -80,7 +81,9 @@ class PreviewGenerator extends stream.Transform{
             {name: "sampleRate", data: 0, size: 4},
             {name: "byteRate", data: 0, size: 4},
             {name: "blockAlign", data: 0, size: 2},
-            {name: "bitsPerSample", data: 0, size: 2}
+            {name: "bitsPerSample", data: 0, size: 2},
+            {name: "dataSectionId", data: 0, size: 4, constraint: "data"},
+            {name: "dataLength", data: 0, size: 4}
         ]
     }
     _readHeader(octet){
@@ -89,9 +92,9 @@ class PreviewGenerator extends stream.Transform{
             section.data += String.fromCharCode(octet);
         }
         else{
-            section.data &= byte << (section.size - this.headerOffset);
+            section.data |= octet << this._headerOffset * 8;
         }
-        ++this.headerOffset;
+        ++this._headerOffset;
         if(this._headerOffset === section.size){
             ++this._headerSection;
             this._headerOffset = 0;
@@ -99,41 +102,55 @@ class PreviewGenerator extends stream.Transform{
         if(this._headerSection === this._header.length){
             const processedHeader = {};
             for(let i = 0; i < this._header.length; ++i){
-                this.processedHeader[this._header[i].name] = this._header[i];
+                processedHeader[this._header[i].name] = this._header[i];
             }
+            this._header = processedHeader;
+            this._samplesPerWindow = Math.ceil(
+                this._header.dataLength.data / this._header.blockAlign.data /
+                this._header.numChannels.data / this._resolution /
+                 this._windowResolution);
+            this._maxAmplitude = 0;
+            for(let i = 0; i < this._header.bitsPerSample.data - 1; ++i){
+                this._maxAmplitude |= 1 << i;
+            }
+            this._twoComplement = Math.pow(2, this._header.bitsPerSample.data);
             this.phase = "body";
         }
     }
     _transform(data, encoding, callback){
-        switch(this.phase){
-            case 'header':
-                for(let i = 0; i < data.length; ++i){
-                    this._readHeader(data[i]);
-                }
-                break;
-            case 'body':
-                for(let i = 0; i < data.length; ++i){
-                    this._readBody(data[i]);
-                }
-                break;
+        for(let i = 0; i < data.length; ++i){
+            if(this.phase == "body"){
+                this._readBody(data[i]);
+            }
+            else{
+                this._readHeader(data[i]);
+            }
         }
         callback(null, data);
     }
-    _readBody(){
-        const bytes = data;
-        for(let i = 0; i < bytes.length; ++i){
-            this._sample &= bytes[i] << 8 * this_byteNumber;
-            if(++this._byteNumber == this._bytesPerSample){
-                this._windowMax = Math.max(this._windowMax, this._sample);
-                this._byteNumber = 0;
-                this._sample = 0;
-                if(++this._sampleNumber == this._samplesPerWindow){
-                    this._windows[this._windowIdx] = this._windowMax;
-                    this._sampleNumber = 0;
-                    if(++this._windowIdx == this._subWindowsResolution){
-                        this.results.push(this._averageSubWindows());
-                        this._windowIdx = 0;
-                    }
+    _readBody(octet){
+        if(this._octetNum === undefined){
+            this._octetNum = 0;
+        }
+        else{
+            this._octetNum += 1;
+        }
+        this._sample |= octet <<  8 * this._byteNumber;
+        if(++this._byteNumber == this._header.blockAlign.data / this._header.numChannels.data){
+            if(this._sample > this._maxAmplitude){
+                this._sample = -1 *(this._twoComplement - this._sample);
+            }
+            console.log(this._sample / this._maxAmplitude);
+            this._windowMax = Math.max(this._windowMax, this._sample);
+            this._byteNumber = 0;
+            this._sample = 0;
+            if(++this._sampleNumber == this._samplesPerWindow){
+                this._windows[this._windowIdx] = this._windowMax;
+                this._windowMax = 0;
+                this._sampleNumber = 0;
+                if(++this._windowIdx == this._windowResolution){
+                    this.results[this._resultIdx++] = this._averageSubWindows();
+                    this._windowIdx = 0;
                 }
             }
         }
@@ -142,7 +159,7 @@ class PreviewGenerator extends stream.Transform{
     
     _averageSubWindows(){
         return this._windows.reduce((accum, el)=> accum + el) /
-            this._subWindowsResolution;
+            this._windowResolution / this._maxAmplitude;
     }
 }
 function generate_waveform(){
@@ -173,7 +190,7 @@ function generate_waveform(){
 
     p.on('finish', ()=>{
         debugger;
-        console.log(p.result)
+        console.log(p.results)
     });
 }
 generate_waveform();
